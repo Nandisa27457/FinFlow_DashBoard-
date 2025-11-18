36842acc35823de7a4e5d9326492a29aa6ff8269
Nandisa <shangenandisa@gmail.com>
2025-11-18 18:18:02 +0200

    Added entries page

---
+++ src/Pages/Entries.tsx
@@ -0,0 +1,13 @@
+import Layout from "../Components/Layout";
+
+
+export default function Entries(){
+    return(
+        <>
+        <Layout/>
+       <h1>This your Entries Page</h1>
+        </>
+        
+        
+    )
+}
