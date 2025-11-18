7533967c582d2da76a4c1595d3e0e40ba1a4c238
Nandisa <shangenandisa@gmail.com>
2025-11-18 18:17:19 +0200

    Added an accounts page

---
+++ src/Pages/Account.tsx
@@ -0,0 +1,13 @@
+import Layout from "../Components/Layout";
+
+
+export default function Account(){
+    return(
+        <>
+        <Layout/>
+       <h1>This your Chart of Accounts Page</h1>
+        </>
+        
+        
+    )
+}
